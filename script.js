e84b52b0881e6b61a0e4b9cb3eef3d2f27a64582
Yureva Lyubov <yureva.exceedteam@gmail.com>
2022-04-01 15:00:54 +0300

    Create delete elements

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let allFinances = [];
 let sum = 0;
+let newObj = {};
+let inputName = null;
+let inputMoney = null;
 
 window.onload = async () => {
   const res = await fetch("http://localhost:8000/allFinance", {
@@ -12,6 +15,10 @@ window.onload = async () => {
 
 const render = () => {
   const contentPage = document.querySelector(".contentPage");
+  while (contentPage.firstChild) {
+    contentPage.removeChild(contentPage.firstChild);
+    sum = 0;
+  }
   const p = document.querySelector(".sum");
   allFinances.forEach((el, idx) => {
     sum += el.money;
@@ -25,7 +32,7 @@ const render = () => {
     contentBox.appendChild(nameCompany);
 
     const date = document.createElement("p");
-    date.textContent = convertDate(el.date.slice(0, 10));
+    date.textContent = convertDate(el.date);
     contentBox.appendChild(date);
 
     const money = document.createElement("p");
@@ -41,20 +48,50 @@ const render = () => {
 
     const deleteIcon = document.createElement("img");
     deleteIcon.src = "image/trash.svg";
+    deleteIcon.addEventListener("click", () => onClickDelete(idx));
     iconBox.appendChild(deleteIcon);
 
     contentBox.appendChild(iconBox);
     contentPage.appendChild(contentBox);
   });
-  p.textContent = `Итого: ${sum} р.`;
+  if (allFinances.length) p.textContent = `Итого: ${sum} р.`;
 };
 
 const convertDate = (date) => {
-  let newDate = "";
-  const dateArr = date.split("-").reverse();
-  dateArr.forEach((el, idx) => {
-    newDate += el;
-    if (idx !== dateArr.length - 1) newDate += ".";
+  return date.slice(0, 10).split("-").reverse().join(".");
+};
+
+const onChangeInput = () => {
+  inputName = document.getElementById("where");
+  inputMoney = document.getElementById("howMany");
+  newObj.nameCompany = inputName.value;
+  newObj.money = Number(inputMoney.value);
+};
+
+const onClickAdd = async () => {
+  if (!newObj.nameCompany) return alert("Enter value name");
+  if (!newObj.money) return alert("Enter value money");
+  const resp = await fetch("http://localhost:8000/createFinance", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(newObj)
   });
-  return newDate;
+  const result = await resp.json();
+  allFinances.push(result);
+  inputName.value = "";
+  inputMoney.value = "";
+  render();
+};
+
+const onClickDelete = async (index) => {
+  const answer = confirm("Are you sure?");
+  if (!answer) return;
+  await fetch(`http://localhost:8000/deleteFinance?id=${allFinances[index]._id}`, {
+    method: "DELETE",
+  });
+  allFinances.splice(index, 1);
+  render();
 };
